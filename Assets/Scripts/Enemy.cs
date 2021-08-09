cf472771c7e17d1cbdede19a47be55db59957ffc
Bary Pollack <bary.pollack@baker.edu>
2021-08-09 00:02:12 -0700

    Initial Commit 51

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.down);
+        if (transform.position.y < -4f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+                player.Damage();
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.up);
+        if (transform.position.y > 8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
