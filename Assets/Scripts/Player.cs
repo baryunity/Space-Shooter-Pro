b3912f72127887ede96ebc59458a6d5d1011787d
Bary Pollack <bary.pollack@baker.edu>
2021-08-09 01:21:18 -0700

    End of slide 55; enemies never stop spawning
    Bug...

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,11 +13,18 @@ public class Player : MonoBehaviour
     private float _canFire = -1f;
     [SerializeField]
     private int _lives = 3;
+    private SpawnManager _spawnManager;
 
     void Start()
     {
         // Set the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL");
+        }
     }
 
     void Update()
@@ -62,6 +69,7 @@ public class Player : MonoBehaviour
         _lives--;
         if (_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private float _waitTime = 5f;
+    [SerializeField]
+    private bool _stopSpawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (! _stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(_waitTime);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
