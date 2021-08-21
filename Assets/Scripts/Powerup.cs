d7bdca2e3f7eb529328d36a54891ec5659dc5edd
Bary Pollack <bary.pollack@baker.edu>
2021-08-20 23:11:52 -0700

    End of slide 66; Enable Trip Shot Powerup Behavior

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_speed * Vector3.down * Time.deltaTime);
+        if (transform.position.y < -4.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            Destroy(this.gameObject);
+    }
+}
