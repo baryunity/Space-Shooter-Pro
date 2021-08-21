e47d6ec12b41db5714438813835902a417ce26be
Bary Pollack <bary.pollack@baker.edu>
2021-08-20 23:45:39 -0700

    End slide 71; Animate Triple Shot Powerup

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -12,6 +12,10 @@ public class Laser : MonoBehaviour
         transform.Translate(_speed * Time.deltaTime * Vector3.up);
         if (transform.position.y > 8f)
         {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
             Destroy(this.gameObject);
         }
     }
