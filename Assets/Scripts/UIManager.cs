df488d69c86e9c1ed38be4f7b11fb03608e6a38a
Bary Pollack <bary.pollack@baker.edu>
2021-08-22 20:41:31 -0700

    End of slide 99; Game Over Text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,11 +12,14 @@ public class UIManager : MonoBehaviour
     private Image _LivesImg;
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,5 +31,10 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _LivesImg.sprite = _liveSprites[currentLives];
+
+        if (currentLives <= 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+        }
     }
 }
