adf4ef9c84d55ae10a35a8b5f519605ffcd74a75
Bary Pollack <bary.pollack@baker.edu>
2021-09-01 00:00:00 -0700

    Asteroid Speed 23f

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     [SerializeField]
-    private float _rotateSpeed = 13.5f;
+    private float _rotateSpeed = 23f;
     [SerializeField]
     private GameObject _explosionPrefab;
     [SerializeField]
